9cd9afef2cf3e08cfa4432efbde66a5da37d645f
Andrew Walker <a.walker@leeds.ac.uk>
2017-01-06 20:39:21 +0000

    Tidy up fundamental.py

--- pytasa/fundamental.py
+++ pytasa/fundamental.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 import numpy as np
 
+
 def _velo(X,rh,C):
     """PHASE-VELOCITY SURFACES IN AN ANISOTROPIC MEDIUM
        revised April 1991
@@ -37,7 +38,7 @@ def _velo(X,rh,C):
 
     return V, EIGVEC
 
-    
+
 def _is_isotropic(C,tol):
     """Are we isotropic - assume matrix is symmetrical at this point."""
     l = (abs(C[0,0]-C[1,1]) < tol) and (abs(C[0,0]-C[2,2]) < tol) and \
@@ -72,7 +73,7 @@ def _cart2(inc, azm):
     X = X/r 
     return X
 
-    
+
 def _V_rot_gam(V,gam):
     """docstring for V_rot_gam"""
     #  Make rotation matrix
@@ -89,8 +90,8 @@ def _V_rot_bet(V,bet):
     RR = [ [np.cos(b), 0, -np.sin(b)] , [0, 1, 0], [np.sin(b), 0, np.cos(b)]] 
     VR = np.dot(V,RR) 
     return VR
-    
-    
+
+
 def _rayvel(Cin,SN,rho):
     """
     Calculate the ray-velocity vector corresponding to a slowness vector.
@@ -103,19 +104,18 @@ def _rayvel(Cin,SN,rho):
     
     returns VG: Group velocity vector (3)    
     """
-    
     C = Cin/rho
-    
+
     ijkl = np.array([[0,5,4],
                      [5,1,3],
                      [4,3,2]])
-      
+
     gamma = np.array([[ SN[0],   0.0,   0.0,   0.0, SN[2], SN[1]],
                       [   0.0, SN[1],   0.0, SN[2],   0.0, SN[0]],
                       [   0.0,   0.0, SN[2], SN[1], SN[0],  0.0]])
 
     cp = np.dot(np.dot(gamma,C),np.transpose(gamma))
-    
+
     dcp = np.zeros((3,3,3))
     for r in range(3):
         for i in range(3):
@@ -131,30 +131,30 @@ def _rayvel(Cin,SN,rho):
                     n=ijkl[k,l]
                     y = y+C[m,n]*SN[k]
                 dcp[r,i,l]=x+y
-                
+
     det=np.zeros(3)            
-                
+
     for r in range(3):
         for m in range(3):
             a = cp - np.identity(3)
             for i in range(3):
                 a[i,m] = dcp[r,i,m]
             det[r]= det[r]+np.linalg.det(a)
-                
+
     den = 0.0
     VG = np.zeros(3)
     for i in range(3):
         den = den + SN[i]*det[i]
-    
+
     for i in range(3):
         VG[i] = det[i]/den    
-        
+
     return VG
 
 
 def phasevels(Cin,rh,incl,azim,vecout=False):
     """Calculate the group velocity details for an elsticity matrix. 
-        
+
         Usage: 
             VGP, VGS1, VGS2, PE, S1E, S2E = groupvels( Cin,rh,incl,azim )                    
                 Calculate group velocity vectors from elasticity matrix C (in GPa) and
@@ -162,7 +162,8 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
                 an inclination and azimuth (both in degrees). Additionally P, S1 and
                 S2-wave polarisations are output in vector form.
         
-            VGP, VGS1, VGS2, PE, S1E, S2E, SNP, SNS1, SNS2, VPP, VPS1, VPS2 = groupvels( Cin,rh,incl,azim,slowout=True )                 
+            VGP, VGS1, VGS2, PE, S1E, S2E, SNP, SNS1, SNS2, VPP, VPS1, VPS2 = 
+                                             groupvels( Cin,rh,incl,azim,slowout=True )
                 Additionally output P, S1 and S2-wave slownesses (SNP, ...) and 
                 phase velocities (VPP, ...) in vector form, as calculated by phasevels.
         
@@ -171,28 +172,27 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
             Based on original fortran code by Mike Kendall and Sean Guest as part of ATRAK.
             Converted to Python by Alan Baird.
     """
-    
-    #copy C to avoid mutating input
+    # Copy C to avoid mutating input
     C=Cin.copy()
-    
-    #Convert inc and azi to arrays of at least 1 dimension otherwise can't iterate over scalars
-    #rh=rho
+
+    # Convert inc and azi to arrays of at least 1 dimension otherwise can't iterate over scalars
+    # rh=rho
     inc=np.atleast_1d(incl)
     azi=np.atleast_1d(azim)
-    
+
     if (np.size(inc)!=np.size(azi)):
         raise ValueError("AZI and INC must be scalars or vectors of the same dimension")
-        
+
     isotol = np.sqrt(np.spacing(1)); # Mbars
-    
+
     #Check that C is valid (if check not suppressed)
     #MS_checkC(C);
-      
+
     #  ** convert GPa to MB file units (Mbars), density to g/cc
     C[:] = C[:] * 0.01
-    
+
     rh = rh / 1e3 
-    
+
     avs = np.zeros(np.size(azi))
     vp  = np.zeros(np.size(azi))
     vs1 = np.zeros(np.size(azi))
@@ -200,12 +200,12 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
     pol = np.zeros(np.size(azi))
     S1P = np.zeros((np.size(azi),3)) 
     S2P = np.zeros((np.size(azi),3)) 
-    
+
     # Eigenvectors
     PE  = np.zeros((np.size(azi),3)) 
     S1E = np.zeros((np.size(azi),3)) 
     S2E = np.zeros((np.size(azi),3)) 
-    
+
     # Cartesion propagation vectors
     XIS = np.zeros((np.size(azi),3)) 
 
@@ -221,19 +221,18 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
         # Compute phase velocities      
         V,EIGVEC=_velo(XI,rh,C)
         #print 'V',V
-        
+
         # pull out the eigenvectors
         P  = EIGVEC[:,0]
         S1 = EIGVEC[:,1]
         S2 = EIGVEC[:,2]
-        
+
         PE[ipair,:]  = P
         S1E[ipair,:] = S1
         S2E[ipair,:] = S2
 
-          
         # calculate projection onto propagation plane      
-        
+
         S1N = np.cross(XI,S1)
         S1P[ipair,:] = np.cross(XI,S1N)
         S2N = np.cross(XI,S2)
@@ -242,7 +241,7 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
         #rotate into y-z plane to calculate angles
         #     (use functions optimised for the two needed 
         #      rotations, see below).
-        
+
         S1PR  = _V_rot_gam(S1P[ipair,:],cazi) 
         S1PRR = _V_rot_bet(S1PR,cinc) 
 
@@ -262,19 +261,17 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
         vs2[ipair] = V[2]
         pol[ipair] = ph
 
-
     # If any directions have zero avs (within machine accuracy)
     # set pol to np.nan - array wise:
     # make sure invalid error warnings are suppressed.
     with np.errstate(invalid='ignore'):
         isiso = (avs > np.sqrt(np.spacing(1))).astype(float) # list of 1.0 and 0.0.
         pol = pol * np.divide(isiso,isiso) # times by 1.0 or np.nan. 
-        
+
         S1P[:,0] = S1P[:,0] * np.divide(isiso,isiso)
         S1P[:,1] = S1P[:,1] * np.divide(isiso,isiso)
         S1P[:,2] = S1P[:,2] * np.divide(isiso,isiso)
-    
-    
+
     # if polout:
     #     return pol,avs,vs1,vs2,vp,S1P,S2P,PE,S1E,S2E,XIS
     if vecout:
@@ -291,24 +288,22 @@ def phasevels(Cin,rh,incl,azim,vecout=False):
 
 def groupvels(Cin,rh,incl,azim,slowout=False):
     """Calculate the group velocity details for an elsticity matrix."""
-    
-    #copy C to avoid mutating input
+
+    # copy C to avoid mutating input
     C=Cin.copy()
-    
-    
+
     #Convert inc and azi to arrays of at least 1 dimension otherwise can't iterate over scalars
     inc=np.atleast_1d(incl)
     azi=np.atleast_1d(azim)
-    
+
     if (np.size(inc)!=np.size(azi)):
         raise ValueError("AZI and INC must be scalars or vectors of the same dimension")
-        
 
     isotol = np.sqrt(np.spacing(1)); # Mbars
-    
+
     # pol,avs,vs1,vs2,vp,S1P,S2P,PE,S1E,S2E,XIS = phasevels(Cin,rh,inc,azi,polout=True)
     pol,avs,vs1,vs2,vp,PE,S1E,S2E,VPP,VPS1,VPS2,SNP,SNS1,SNS2 = phasevels(Cin,rh,inc,azi,vecout=True)
-    
+
     #  ** convert density to g/cc
 
     rh = rh / 1e3
@@ -317,7 +312,6 @@ def groupvels(Cin,rh,incl,azim,slowout=False):
     VGP  = np.zeros((np.size(azi),3)) 
     VGS1 = np.zeros((np.size(azi),3)) 
     VGS2 = np.zeros((np.size(azi),3))
-    
 
     #start looping
     for ipair in range(np.size(inc)):
@@ -327,26 +321,21 @@ def groupvels(Cin,rh,incl,azim,slowout=False):
         VGS1[ipair,:] = _rayvel(C,SNS1[ipair,:],rh)
         VGS2[ipair,:] = _rayvel(C,SNS2[ipair,:],rh)
 
-        
-
     if slowout:
         return VGP, VGS1, VGS2, PE, S1E, S2E, SNP, SNS1, SNS2, VPP, VPS1, VPS2
     else:
         return VGP, VGS1, VGS2, PE, S1E, S2E
-    
-# FIXME: this is a good place to put things like phase velocity 
-#        calculation or tensor rotation. Basic rule should be that
-#        the functions take a 6x6 np. array (or matrix?) as input
-#        and return something. We can then build an OO interface on top
+
 
 def cij_stability(cij):
-    """Check that the elastic constants matrix is positive
-    definite - i.e. that the structure is stable to small
-    strains. This is done by finding the eigenvalues by
-    diagonalization and checking that they are all positive.
-    See Born & Huang, "Dynamical Theory of Crystal Lattices"
-    (1954) page 141."""
+    """Check that the elastic constants matrix is positive definite 
 
+    That is,  check that the structure is stable to small strains. This
+    is done by finding the eigenvalues of the Voigt elastic stiffness matrix
+    by diagonalization and checking that they are all positive.
+
+    See Born & Huang, "Dynamical Theory of Crystal Lattices" (1954) page 141.
+    """
     stable = False
     (eigenvalues, eigenvectors) = np.linalg.eig(cij)
     if (np.amin(eigenvalues) > 0.0):
@@ -385,9 +374,6 @@ def invert_cij(cij, ecij):
     its inverse (the elastic compliance matrix, cij), but should
     work for any rank 2 square matrix.
     """
-
-    # Assuming we have a rank 2 square array
-    # of the same size for input array.
     if (np.ndim(cij) != 2):
         raise ValueError("Matrix must be rank 2")
     if (np.shape(cij)[0] != np.shape(cij)[1]):
