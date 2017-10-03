d971daa5ce964752ce2413ae208babcb80da13b3
Jack Walpole <j.walpole@gmail.com>
2017-10-03 01:17:16 +0100

    Setup __init__.py and get the code usable in an interactive session

---
+++ pytasa/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from .fundamental import phasevels, groupvels, cij_stability, invert_cij
+from .anisotropy_index import zenerAniso, uAniso
+from . import io, polycrystal
